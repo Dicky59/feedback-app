b5cbdf5e4572b6f2a38482a0e392936bf3663425
Dicky59 <pekkaarvolaine@gmail.com>
2025-10-17 16:12:41 +0300

    feat(frontend): initialize React + Vite + TypeScript project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Feedback App</h1>
+        <p>Welcome to our feedback application!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
